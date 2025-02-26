bd61302f2a1a4104b7b69b2f6ec058e4c488a84f
Abhi Joshi <abhijoshi783@gmail.com>
2025-02-26 07:01:23 +0000

    feat: add useFollow hook to follow or unfollow user

--- frontend/src/components/common/RightPanel.tsx
+++ frontend/src/components/common/RightPanel.tsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { IAuthUser } from "../../types/auth";
+import useFollow from "../../hooks/useFollow";
 
 const RightPanel = () => {
-  const isLoading = false;
+  const { data: suggestedUsers, isLoading } = useQuery<Array<IAuthUser>>({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      const res = await axios.get(
+        "http://localhost:3000/api/v1/user/suggested",
+        { withCredentials: true }
+      );
+
+      if (res) {
+        return res.data;
+      }
+    },
+  });
+
+  if (suggestedUsers?.length === 0) {
+    return <div className="md:w-64 w-0"></div>;
+  }
+
+  const { followUnfollow } = useFollow();
 
   return (
     <div className="hidden lg:block my-4 mx-2">
       <div className="bg-[#16181C] p-4 rounded-md sticky top-2">
         <p className="font-bold">Who to follow</p>
         <div className="flex flex-col gap-4">
-          {/* item */}
           {isLoading && (
             <>
               <RightPanelSkeleton />
@@ -20,7 +40,8 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers &&
+            suggestedUsers.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
@@ -44,7 +65,10 @@ const RightPanel = () => {
                 <div>
                   <button
                     className="btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      followUnfollow(user._id);
+                    }}
                   >
                     Follow
                   </button>
---
+++ frontend/src/hooks/useFollow.ts
@@ -0,0 +1,46 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const useFollow = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: followUnfollow, isPending } = useMutation({
+    mutationFn: async (userId: string) => {
+      const res = await axios.get(
+        `http://localhost:3000/api/v1/user/follow/${userId}`,
+        { withCredentials: true }
+      );
+
+      if (res) {
+        return res.data;
+      }
+    },
+    onSuccess: (data) => {
+      if (data.msg) {
+        toast.success(data.msg);
+      }
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+      ]);
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          const message =
+            error.response?.data.message || error.response?.data.error;
+          toast.error(message);
+        } else {
+          toast.error(error.message);
+        }
+      } else {
+        console.log(error);
+      }
+    },
+  });
+
+  return { followUnfollow, isPending };
+};
+
+export default useFollow;
